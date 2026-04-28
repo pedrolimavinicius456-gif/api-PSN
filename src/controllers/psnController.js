40ca4900bac9dabb67cf6bd4e2f2b7ef2d8ebabd
pedrolimavinicius456-gif <pedro.limavinicius456@gmail.com>
2026-04-28 10:23:35 -0300

    Add files via upload

--- src/controllers/psnController.js
+++ src/controllers/psnController.js
@@ -1,13 +1,13 @@
-import { PSN } from "../data/psn.Data.js";
+import db from "../data/psn.Data.js";
 
 export const listarJogos = (req, res) => {
-    res.status(200).json(PSN)
+    res.status(200).json(db.data.PSN)
 };
 
 export const buscarJogoPorId = (req, res) =>{
     const { id } = req.params;
 
-    const psn = PSN.find(p => p.id === Number(id));
+    const psn = db.data.PSN.find(p => p.id === Number(id));
 
     if(!psn) {
         return res.status(404).json({mensagem: "Jogo não encontrado!" });
@@ -16,29 +16,30 @@ export const buscarJogoPorId = (req, res) =>{
     res.status(200).json(psn);
 };
 
-export const criarJogo = (req, res) => {
+export const criarJogo = async (req, res) => {
 
     const {nome, criador, ano, categoria} = req.body;
 
-    const novoJogo = {id: PSN.length + 1, nome, criador, ano, categoria};
+    const novoJogo = {id: db.data.PSN.length + 1, nome, criador, ano, categoria};
 
-    PSN.push(novoJogo);
+    db.data.PSN.push(novoJogo);
+    await db.write()
     res.status(201).json(novoJogo);
 }
 
 export const alterarJogo =  (req, res) => {
     const { id } = req.params;
-    const index = PSN.findIndex(p => p.id === Number(id));
+    const index = db.data.PSN.findIndex(p => p.id === Number(id));
 
     if(index === -1) return res.status(404).json({ mensagem:"Jogo não encontrado" })
     
-    PSN[index] = {id: Number(id), ...req.body };
+    db.data.PSN[index] = {id: Number(id), ...req.body };
     res.json(PSN[index]);
 }
 
-export const alterarCampoJogo = (req, res) => {
+export const alterarCampoJogo = async (req, res) => {
     const { id } = req.params;
-    const psn = PSN.find(p => p.id === Number(id));
+    const psn = db.data.PSN.find(p => p.id === Number(id));
 
     if(!psn) return res.status(404).json({ mensagem: "Jogo não encontrado"});
     
@@ -47,7 +48,7 @@ export const alterarCampoJogo = (req, res) => {
     if(req.body.criador) psn.criador = req.body.criador;
     if(req.body.ano) psn.ano = req.body.ano;
     if(req.body.categoria) psn.categoria = req.body.categoria;
-
+    await db.write();
     res.json(psn);
 }
 
